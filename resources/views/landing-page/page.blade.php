ed985c0fda068af0b27220f2e5dbe62717a270b8
hiarief <arhistr14@gmail.com>
2026-02-10 15:27:56 +0700

    */image-desa-landing-page

--- resources/views/landing-page/page.blade.php
+++ resources/views/landing-page/page.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('assets/img/desa.png') }}" type="image/x-icon" />
     <title>DesaBisa - Kembali Ke Desa</title>
     @include('landing-page.partials.styles')
 </head>
